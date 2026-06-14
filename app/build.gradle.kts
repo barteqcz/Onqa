a8174c36387d50d3f6b8232ca75ccdc09ce45115
barteqcz <bartosz.brom06@gmail.com>
2026-06-14 18:18:04 +0200

    Fixed the fade under the mini-player when scrolling up from the end of the station list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 1
-        versionName = "2.0.2"
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
